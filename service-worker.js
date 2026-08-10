9562966f16b7dc78ca29c014a7b39d35e6877c66
Mohammad Rifat Hossain <160498410+Rifat24865@users.noreply.github.com>
2026-08-11 02:08:26 +0600

    Refactor service worker caching logic

--- service-worker.js
+++ service-worker.js
@@ -8,55 +8,71 @@ const FILES_TO_CACHE = [
     "./manifest.json"
 ];
 
+
+/* INSTALL */
+
 self.addEventListener("install", event => {
+
     event.waitUntil(
+
         caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(FILES_TO_CACHE))
-            .then(() => self.skipWaiting())
+            .then(cache => {
+
+                return cache.addAll(FILES_TO_CACHE);
+
+            })
+
     );
+
+    self.skipWaiting();
+
 });
 
+
+/* ACTIVATE */
+
 self.addEventListener("activate", event => {
+
     event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
+
+        caches.keys().then(keys => {
+
+            return Promise.all(
+
                 keys
                     .filter(key => key !== CACHE_NAME)
                     .map(key => caches.delete(key))
-            )
-        ).then(() => self.clients.claim())
+
+            );
+
+        })
+
     );
+
+    self.clients.claim();
+
 });
 
+
+/* FETCH */
+
 self.addEventListener("fetch", event => {
+
     event.respondWith(
+
         caches.match(event.request)
             .then(cachedResponse => {
+
                 if (cachedResponse) {
+
                     return cachedResponse;
+
                 }
 
-                return fetch(event.request)
-                    .then(networkResponse => {
-                        if (
-                            !networkResponse ||
-                            networkResponse.status !== 200 ||
-                            networkResponse.type !== "basic"
-                        ) {
-                            return networkResponse;
-                        }
+                return fetch(event.request);
 
-                        const responseToCache = networkResponse.clone();
-
-                        caches.open(CACHE_NAME).then(cache => {
-                            cache.put(event.request, responseToCache);
-                        });
-
-                        return networkResponse;
-                    })
-                    .catch(() => {
-                        return caches.match("./index.html");
-                    });
             })
+
     );
+
 });
