7d0a8915ff21ab0dac41e436c8c889acf83c2deb
Mohammad Rifat Hossain <160498410+Rifat24865@users.noreply.github.com>
2026-08-11 00:34:27 +0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = "friends-dictionary-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        ).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(cachedResponse => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return fetch(event.request)
+                    .then(networkResponse => {
+                        if (
+                            !networkResponse ||
+                            networkResponse.status !== 200 ||
+                            networkResponse.type !== "basic"
+                        ) {
+                            return networkResponse;
+                        }
+
+                        const responseToCache = networkResponse.clone();
+
+                        caches.open(CACHE_NAME).then(cache => {
+                            cache.put(event.request, responseToCache);
+                        });
+
+                        return networkResponse;
+                    })
+                    .catch(() => {
+                        return caches.match("./index.html");
+                    });
+            })
+    );
+});
